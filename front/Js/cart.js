746eb81966634af77b9c2377ccd95f07f652a0e9
Reservia <lavalleejimcomptepro@gmail.com>
2022-02-23 14:54:24 +0100

    commit cart

--- front/Js/cart.js
+++ front/Js/cart.js
@@ -1,17 +1,32 @@
-// localstorage
+// produits envoyer dans le localstorage
 
 function savecart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart))
 }
 
-// récuperation des produits du localStorage 
+// produits récupérer dans le localStorage 
 
 function getcart() {
     let cart = localStorage.getItem("cart")
     if (cart == null) {
         return []
     } else {
-        return json(cart)
+        return JSON.parse(cart)
     }
+}
+
+// produit choisis ajouté dans le localStorage et verification des doublons 
+
+function addcart(product) {
+    let cart = getcart()
+    let foundProduct = cart.find(p => p.id == product.id) && cart.find(p => p.color == product.color)
+    if (foundProduct != undefined) {
+        foundProduct.quantity += product.quantity
+    } else {
+
+        cart.push(product)
+    }
+
+    savecart(cart)
 }
 
