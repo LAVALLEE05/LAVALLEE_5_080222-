8e59259592c63f0bd32565565d38a1e69db45975
Reservia <lavalleejimcomptepro@gmail.com>
2022-02-23 14:47:40 +0100

    commit cart

--- front/Js/cart.js
+++ front/Js/cart.js
@@ -1,33 +1,5 @@
-let cart = getproduct
+// localstorage
 
-if(product.length ==  0){
-    alert("votre panier est vide")
+function savecart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart))
 }
-
-for (let cart of product) {
-
-    document.querySelector("#cart__items").innerHTML +=
-
-        `<article class="cart__item" data-id="${product.id}" data-color="${product.color} ">
-                            <div class="cart__item__img">
-                              <img src="${product.image}" alt="${product.altTxt} ">
-                            </div>
-                            <div class="cart__item__content">
-                              <div class="cart__item__content__description">
-                                <h2>${product.name} </h2>
-                                <p>${product.color}</p>
-                                <p>${product.price}€</p>
-                              </div>
-                              <div class="cart__item__content__settings">
-                                <div class="cart__item__content__settings__quantity">
-                                  <p>Qté :  </p>
-                                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                                </div>
-                                <div class="cart__item__content__settings__delete">
-                                  <p class="deleteItem">Supprimer</p>
-                                </div>
-                              </div>
-                            </div>
-                          </article>`
-}
-
