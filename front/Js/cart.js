17ae3a8db0ddb8a1f15a5df8492a145947608a71
Reservia <lavalleejimcomptepro@gmail.com>
2022-03-28 10:05:21 +0200

    update

--- front/Js/cart.js
+++ front/Js/cart.js
@@ -12,29 +12,28 @@ for (let product of basket) {
 
     document.querySelector("#cart__items").innerHTML +=
 
-        `<article class="cart__item" data-id="${product.id}" data-color="${product.color} ">
-                            <div class="cart__item__img">
-                              <img src="${product.image}" alt="${product.altTxt} ">
-                            </div>
-                            <div class="cart__item__content">
-                              <div class="cart__item__content__description">
-                                <h2>${product.name} </h2>
-                                <p>${product.color}</p>
-                                <p>${product.price}€</p>
-                              </div>
-                              <div class="cart__item__content__settings">
-                                <div class="cart__item__content__settings__quantity">
-                                  <p>Qté :  </p>
-                                  <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                                </div>
-                                <div class="cart__item__content__settings__delete">
-                                  <p class="deleteItem">Supprimer</p>
-                                </div>
-                              </div>
-                            </div>
-                          </article>`
-}                     
-
+    `<article class="cart__item" data-id="${product.id}" data-color="${product.color} ">
+    <div class="cart__item__img">
+      <img src="${product.image}" alt="${product.altTxt} ">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${product.name} </h2>
+        <p>${product.color}</p>
+        <p>${product.price}€</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté :  </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>`
+}
 // suppression du produit au click 
 
 document.querySelectorAll(".deleteItem").forEach(item => item.addEventListener("click", (e) => {
