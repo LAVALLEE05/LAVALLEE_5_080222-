19cb90454525437737a7e5d7312b1a10ae2e4324
Reservia <lavalleejimcomptepro@gmail.com>
2022-04-06 10:04:48 +0200

    update

--- front/Js/confirmation.js
+++
@@ -1,9 +0,0 @@
-// recapitulatif de la commande
-
-let params = new URL(document.location).searchParams;
-let id = params.get("id");
-console.log(id);
-
-const nbOrder = document.getElementById("orderId");
-nbOrder.innerHTML = id
-localStorage.clear()
