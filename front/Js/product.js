156fb101de599fbacb60d0195f89b705a665bbf0
Reservia <lavalleejimcomptepro@gmail.com>
2022-05-11 10:53:13 +0200

    rectification erreur

--- front/Js/product.js
+++ front/Js/product.js
@@ -1,76 +1,84 @@
 // recupération du produit issu de L'url
 
-let productApi ="";
+let productApi = "";
 let id = (new URL(window.location).searchParams.get("id"));
 fetch("http://localhost:3000/api/products/" + id)
-    .then(data => data.json())
+  .then(data => data.json())
 
-// affichage du produit 
+  // affichage du produit 
 
-.then(product => {
+  .then(product => {
     document.querySelector(".item__img").insertAdjacentHTML("afterbegin", `<img src="${product.imageUrl}" alt="${product.altTxt}">`);
     document.querySelector("#title").insertAdjacentHTML("afterbegin", `${product.name}`);
     document.querySelector("#price").insertAdjacentHTML("afterbegin", `${product.price}`);
     document.querySelector("#description").insertAdjacentHTML("afterbegin", `${product.description}`);
     for (let productSelectColor of product.colors) {
-        document.querySelector("#colors").innerHTML += `<option value="${productSelectColor}">${productSelectColor}</option>`
+      document.querySelector("#colors").innerHTML += `<option value="${productSelectColor}">${productSelectColor}</option>`
     }
     productApi = product
-});
+  });
 
 //  produit a envoyer dans le localStorage au click
 
 document.querySelector("#addToCart").addEventListener("click", function () {
 
-    const colors = document.getElementById('colors').value
-    const quantity = document.getElementById('quantity').value
-    const quantityNumber = parseInt(quantity)
-    const productId = {
-        trueId : id,
-        id: id + "_" + colors,
-        name: productApi.name,
-        color: colors,
-        quantity: quantityNumber,
-        price: productApi.price,
-        image: productApi.imageUrl,
-        altTxt: productApi.altTxt,
-    }
+  const colors = document.getElementById('colors').value
+  const quantity = document.getElementById('quantity').value
+  const quantityNumber = parseInt(quantity)
+  const productId = {
+    trueId: id,
+    id: id + "_" + colors,
+    name: productApi.name,
+    color: colors,
+    quantity: quantityNumber,
+    image: productApi.imageUrl,
+    altTxt: productApi.altTxt,
+  }
 
-// alerte en cas de probleme
+  // alerte en cas de probleme
 
-    if (productId.color == "") {
-
-    } else if (productId.quantity <=0) {
-      const warning = document.querySelector(".item__content__settings").insertAdjacentHTML("afterbegin", `<style>
+  if (productId.color == "") {
+    const warning = document.querySelector(".item__content__settings").insertAdjacentHTML("afterbegin", `<style>
       .item__content__settings{
         background-color:red;
       }
       `)
 
-      alert('merci de choisir une quantitée entre 1 et 100')
+    alert('merci de choisir une couleur')
 
-    } else {
+  } else if (productId.quantity <= 0 && productId.quantity > 100) {
+    const warning = document.querySelector(".item__content__settings").insertAdjacentHTML("afterbegin", `<style>
+      .item__content__settings{
+        background-color:red;
+      }
+      `)
 
-// envoi dans le localStorage et redirection vers le panier 
-// appel du panier
+    alert('merci de choisir une quantitée entre 1 et 100')
 
-  if(!localStorage.getItem("basket")) { 
-  localStorage.setItem("basket", JSON.stringify([]));   } 
+  } else {
 
-  let basket = JSON.parse(localStorage.getItem("basket"));
+    // envoi dans le localStorage et redirection vers le panier 
+    // appel du panier
 
-// recherche dans le panier
-        let foundProduct = basket.find(p => p.id == productId.id)        
-        if (foundProduct != undefined) {
-          foundProduct.quantity += productId.quantity
-        }else{
-            basket.push(productId);
-        }    
-
-// sauveagarde panier
-
-        localStorage.setItem("basket", JSON.stringify(basket));
-
-        window.location.assign("cart.html")
+    if (!localStorage.getItem("basket")) {
+      localStorage.setItem("basket", JSON.stringify([]));
     }
+
+    let basket = JSON.parse(localStorage.getItem("basket"));
+
+    // recherche dans le panier
+
+    let foundProduct = basket.find(p => p.id == productId.id)
+    if (foundProduct != undefined) {
+      foundProduct.quantity += productId.quantity
+    } else {
+      basket.push(productId);
+    }
+
+    // sauveagarde panier
+
+    localStorage.setItem("basket", JSON.stringify(basket));
+
+    window.location.assign("cart.html")
+  }
 });
